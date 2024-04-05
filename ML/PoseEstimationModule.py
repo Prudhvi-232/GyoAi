f6d058758fdb662de2325e64190829e64e53946d
Prudhvi-232 <prudhvitaduvai@gmail.com>
2024-04-05 23:33:38 +0530

    poseDetector

--- ML/PoseEstimationModule.py
+++ ML/PoseEstimationModule.py
@@ -2,3 +2,16 @@ import cv2
 import mediapipe as mp
 import time
 import math
+
+class poseDetector():
+    def __init__(self):
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(static_image_mode=False,
+                                      model_complexity=0,
+                                      smooth_landmarks=True,
+                                      enable_segmentation=False,
+                                      smooth_segmentation=True,
+                                      min_detection_confidence=0.5,
+                                      min_tracking_confidence=0.5)
+        self.mpDraw = mp.solutions.drawing_utils
+
