e74d56ce1c67160ff5cf65ea4a472cd81cf810fd
Pranilash <144217030+Pranilash@users.noreply.github.com>
2024-04-07 07:10:06 +0530

    Update hardhat.config.js

--- BlockChain/hardhat.config.js
+++ BlockChain/hardhat.config.js
@@ -1,13 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
 
   networks: {
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/rjPcfyLJX4u64zzXa6xSxB-v2ouNz9dV",
-      accounts: ["5cdf05bc61362b11c52c2426a7ca678f02a788e9a8d612cd482205e131e1e79e"],
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
     },
   },
 };
